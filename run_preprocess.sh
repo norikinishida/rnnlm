555d3569ed2b6e8e4785ae41aedfeea58006c5f1
norikinishida <norikinishida@gmail.com>
2017-02-19 19:38:39 +0900

    fix

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -4,6 +4,8 @@ BASE=/mnt/hdd
 INPUT=$BASE/dataset/Wikipedia/english/corpora/enwiki-latest-articles.xml.corpus
 OUTPUT=$BASE/projects/rnnlm/data/enwiki-latest-articles.xml.corpus.preprocessed
 
-python preprocess.py \
+python nlppreprocess/preprocess.py \
     --input $INPUT \
-    --output $OUTPUT 
+    --output $OUTPUT \
+    --prune_at 300000 \
+    --min_count 5
