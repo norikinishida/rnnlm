068ebe95696055ded5f845e2b906bdbfd6a04a2a
norikinishida <norikinishida@gmail.com>
2017-04-01 19:49:32 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.head_50000.txt
 CORPUS=/mnt/hdd/projects/rnnlm/data/books_large.merge.head_50000.txt.preprocessed
+
+python nlppreprocess/preprocess.py \
+    --input $RAW \
+    --output $CORPUS \
+    --prune_at 300000 \
+    --min_count 5
 
 python scripts/train.py \
     --gpu 0 \
--- run_plotting.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-python plot.py \
-    --log /path/to/rnnlm.enwiki-latest-pages-articles.xml.corpus.preprocessed.experiment_1.log
