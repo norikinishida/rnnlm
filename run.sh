d09d45c58bbabc9e332e2b182dea8f68063fd363
norikinishida <norikinishida@gmail.com>
2017-04-04 13:19:15 +0900

    fix

--- run.sh
+++ run.sh
@@ -3,14 +3,38 @@
 RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.head_50000.txt
 CORPUS=/mnt/hdd/projects/rnnlm/data/books_large.merge.head_50000.txt.preprocessed
 
-python nlppreprocess/preprocess.py \
+TMP=./tmp.txt
+python nlppreprocess/lowercase.py \
     --input $RAW \
+    --output $TMP
+
+# echo "[nlppreprocess;StanfordCoreNLP] Processing ..."
+# rm tmp.properties
+# touch tmp.properties
+# echo "annotators = tokenize, ssplit" >> tmp.properties
+# echo "ssplit.eolonly = true" >> tmp.properties
+# echo "outputFormat = conll" >> tmp.properties
+# echo "file = $TMP" >> tmp.properties
+# java -Xmx10g edu.stanford.nlp.pipeline.StanfordCoreNLP -props tmp.properties
+# python nlppreprocess/conll2lines.py \
+#     --input $TMP.conll \
+#     --output $TMP
+
+python nlppreprocess/tokenizer.py \
+    --input $TMP \
+    --output $TMP
+
+python nlppreprocess/replace_digits.py \
+    --input $TMP \
+    --output $TMP
+
+python nlppreprocess/append_eos.py \
+    --input $TMP \
+    --output $TMP
+
+python nlppreprocess/replace_rare_words.py \
+    --input $TMP \
     --output $CORPUS \
-    --corenlp 1 \
-    --lowercase 1 \
-    --replace_digits 1 \
-    --append_eos 1 \
-    --replace_rare 1 \
     --prune_at 300000 \
     --min_count 5
 
