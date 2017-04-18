635e93f7f375cdf2191bd4f80631b1249ed62174
norikinishida <norikinishida@gmail.com>
2017-04-18 16:41:19 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
 RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.head_50000.txt
-CORPUS_ALL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed
 CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.train
 CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.val
 
 # RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.txt
-# CORPUS_ALL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.txt.preprocessed
 # CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.txt.preprocessed.train
 # CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.txt.preprocessed.val
 
 ##################################
+# preparation
 
 TMP=./tmp.txt
+
 python nlppreprocess/lowercase.py \
     --input $RAW \
     --output $TMP.lowercase
@@ -37,26 +37,33 @@ python nlppreprocess/replace_digits.py \
     --input $TMP.tokenize \
     --output $TMP.replace_digits
 
-python nlppreprocess/replace_rare_words.py \
-    --input $TMP.replace_digits \
-    --output $TMP.replace_rare_words \
-    --prune_at 300000 \
-    --min_count 5
-
 python nlppreprocess/append_eos.py \
-    --input $TMP.replace_rare_words \
-    --output $CORPUS_ALL
+    --input $TMP.replace_digits \
+    --output $TMP.append_eos
 
 python nlppreprocess/split_corpus.py \
-    --all $CORPUS_ALL \
-    --train $CORPUS_TRAIN \
-    --val $CORPUS_VAL \
+    --all $TMP.append_eos \
+    --train $TMP.train \
+    --val $TMP.val \
     --size 5000
 
 python nlppreprocess/create_dictionary.py \
-    --corpus $CORPUS_TRAIN
+    --corpus $TMP.train \
+    --dict $CORPUS_TRAIN.dictionary \
+    --prune_at 300000 \
+    --min_count 5
+
+python nlppreprocess/replace_rare_words.py \
+    --input $TMP.train \
+    --output $CORPUS_TRAIN \
+    --dict $CORPUS_TRAIN.dictionary
+python nlppreprocess/replace_digits.py \
+    --input $TMP.val \
+    --output $CORPUS_VAL \
+    --dict $CORPUS_TRAIN.dictionary
 
 ##################################
+# training
 
 python scripts/train.py \
     --gpu 0 \
