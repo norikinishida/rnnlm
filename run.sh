6490a1babfc3f77af857d54f27a209123c4a872f
norikinishida <norikinishida@gmail.com>
2017-06-07 17:05:51 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.head_50000.txt
-CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.train
-CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.val
+# RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.head_50000.txt
+# CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.train
+# CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.head_50000.txt.preprocessed.val
 
-# RAW=/mnt/hdd/dataset/Book-Corpus/books_large.merge.txt
-# CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.txt.preprocessed.train
-# CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/bookcorpus/books_large.merge.txt.preprocessed.val
+RAW=/mnt/hdd/dataset/Wikipedia/english/corpora/enwiki-latest-pages-articles.xml.corpus
+CORPUS_TRAIN=/mnt/hdd/projects/rnnlm/data/corpora/wikipedia/enwiki-latest-pages-articles.xml.corpus.preprocessed.train
+CORPUS_VAL=/mnt/hdd/projects/rnnlm/data/corpora/wikipedia/enwiki-latest-pages-articles.xml.corpus.preprocessed.val
+
 
 ##################################
 # preparation
