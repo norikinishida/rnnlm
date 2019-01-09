2841b3162d31f5f1b19d6a0776083cf00e94d124
norikinishida <norikinishida@gmail.com>
2019-01-09 14:31:49 +0900

    fix

--- run_rnnlm.sh
+++ run_rnnlm.sh
@@ -3,9 +3,26 @@
 GPU=0
 MODEL=lstmlm
 CONFIG=./config/template.ini
+NAME=trial1
 
 python main.py \
     --gpu ${GPU} \
     --model ${MODEL} \
     --config ${CONFIG} \
+    --name ${NAME} \
     --actiontype train
+
+python main.py \
+    --gpu ${GPU} \
+    --model ${MODEL} \
+    --config ${CONFIG} \
+    --name ${NAME} \
+    --actiontype evaluation
+
+python main.py \
+    --gpu ${GPU} \
+    --model ${MODEL} \
+    --config ${CONFIG} \
+    --name ${NAME} \
+    --actiontype dump_outputs
+
